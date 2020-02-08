45cc44c7b25484fc03616a6ce56f46d139dc79d6
Lauren Stephenson <compscilauren@gmail.com>
2020-02-08 09:42:28 -0600

    Add quiz data filler json

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -2,12 +2,70 @@ import React from 'react';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-import { MonoText } from '../components/StyledText';
 import { Button } from '../components/Button';
+
+const quizData = {
+  quiz: {
+    quiz1: {
+      question1: {
+        correctoption: 'option3',
+        options: {
+          option1: 'Java',
+          option2: 'PHP',
+          option3: 'Javascript',
+          option4: 'IOS',
+        },
+        question: 'React is a ____ library',
+      },
+      question2: {
+        correctoption: 'option4',
+        options: {
+          option1: 'XML',
+          option2: 'YML',
+          option3: 'HTML',
+          option4: 'JSX',
+        },
+        question: '____ tag syntax is used in React',
+      },
+      question3: {
+        correctoption: 'option1',
+        options: {
+          option1: 'Single root DOM node',
+          option2: 'Double root DOM node',
+          option3: 'Multiple root DOM node',
+          option4: 'None of the above',
+        },
+        question: 'Application built with just React usually have ____',
+      },
+      question4: {
+        correctoption: 'option2',
+        options: {
+          option1: 'mutable',
+          option2: 'immutable',
+          option3: 'variable',
+          option4: 'none of the above',
+        },
+        question: 'React elements are ____',
+      },
+      question5: {
+        correctoption: 'option3',
+        options: {
+          option1: 'functions',
+          option2: 'array',
+          option3: 'components',
+          option4: 'json data',
+        },
+        question:
+          'React allows to split UI into independent and reusable pieses of ____',
+      },
+    },
+  },
+};
 
 export default class QuizScreen extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
+    const qdata = quizData.quiz.quiz1;
     return (
       <View style={styles.container}>
         <ScrollView
@@ -19,21 +77,11 @@ export default class QuizScreen extends React.Component {
 
             <View
               style={[styles.codeHighlightContainer, styles.quizScreenFilename]}
-            >
-              <MonoText style={styles.clockText}>Text</MonoText>
-            </View>
+            ></View>
           </View>
 
           <TouchableOpacity onPress={() => navigate('Settings')}>
             <Button title="HELP" />
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => navigate('Settings')}>
-            <Button title="Arrived" />
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => navigate('Settings')}>
-            <Button title="Take Quiz" />
           </TouchableOpacity>
         </ScrollView>
       </View>
