9a331513a6b7add6ca47cd9d65258277e0557602
darkestmidnight <elly.richardson18@gmail.com>
2020-02-09 03:24:52 -0600

    Added sms and call

--- screens/HelpScreen.js
+++ screens/HelpScreen.js
@@ -1,9 +1,95 @@
-import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Button } from '../components/Button';
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Button } from "../components/Button";
+import Geolocation from "react-native-geolocation-service";
+import Geocoder from "react-native-geocoding";
 
 export default class HelpScreen extends React.Component {
+  handleSendSMS = async () => {
+    fetch("http://13.58.89.149:8000/contacts/api/sms/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        receiver: "+18167729435",
+        sender: "+15307224633",
+        message:
+          "I am in need of assistance in this location: 1450 Jayhawk Blvd, Lawrence, KS 66045"
+      })
+    });
+    fetch("http://13.58.89.149:8000/contacts/api/sms/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        receiver: "+18165502253",
+        sender: "+15307224633",
+        message:
+          "I am in need of assistance in this location: 1450 Jayhawk Blvd, Lawrence, KS 66045"
+      })
+    });
+    fetch("http://13.58.89.149:8000/contacts/api/sms/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        receiver: "+19137558051",
+        sender: "+15307224633",
+        message:
+          "I am in need of assistance in this location: 1450 Jayhawk Blvd, Lawrence, KS 66045"
+      })
+    });
+  };
+
+  handleSendCall = async () => {
+    fetch("http://13.58.89.149:8000/contacts/api/call/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        receiver: "+18167729435",
+        sender: "+15307224633",
+        message:
+          "I am in need of assistance in this location: 1450 Jayhawk Blvd, Lawrence, KS 66045"
+      })
+    });
+    fetch("http://13.58.89.149:8000/contacts/api/call/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        receiver: "+19134855706",
+        sender: "+15307224633",
+        message:
+          "I am in need of assistance in this location: 1450 Jayhawk Blvd, Lawrence, KS 66045"
+      })
+    });
+    fetch("http://13.58.89.149:8000/contacts/api/call/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        receiver: "+19137558051",
+        sender: "+15307224633",
+        message:
+          "I am in need of assistance in this location: 1450 Jayhawk Blvd, Lawrence, KS 66045"
+      })
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -18,8 +104,11 @@ export default class HelpScreen extends React.Component {
             </Text>
           </View>
 
-          <TouchableOpacity onPress={() => alert('Location sent! No, really!')}>
-            <Button title="Send My Location Now" />
+          <TouchableOpacity onPress={this.handleSendSMS.bind(this)}>
+            <Button title="Send My Location (SMS)" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.handleSendCall.bind(this)}>
+            <Button title="Send My Location (Call)" />
           </TouchableOpacity>
         </ScrollView>
       </View>
@@ -28,26 +117,26 @@ export default class HelpScreen extends React.Component {
 }
 
 HelpScreen.navigationOptions = {
-  header: null,
+  header: null
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 30
   },
   textContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginHorizontal: 50,
-    marginBottom: 12,
+    marginBottom: 12
   },
   randomText: {
     fontSize: 24,
-    color: 'rgba(96,100,109, 1)',
+    color: "rgba(96,100,109, 1)",
     lineHeight: 24,
-    textAlign: 'center',
-  },
+    textAlign: "center"
+  }
 });
