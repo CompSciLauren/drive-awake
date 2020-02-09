c17fd8b545975fde333829d8707623a19d3df076
Lauren Stephenson <compscilauren@gmail.com>
2020-02-08 18:24:56 -0600

    Finish handle get request function

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -27,6 +27,24 @@ export default class HomeScreen extends Component {
     });
   };
 
+  handleGetRequest = async () => {
+    fetch('http://13.58.89.149:8000/endpoint/api/status/', {
+      method: 'GET',
+      //Request Type
+    })
+      .then(response => response.json())
+      //If response is in json then in success
+      .then(responseJson => {
+        //Success
+        alert('is drowsy: ' + responseJson[responseJson.length - 1].isDrowsy);
+      })
+      //If response is not in json then in error
+      .catch(error => {
+        //Error
+        console.error(error);
+      });
+  };
+
   render() {
     return (
       <View style={{ paddingTop: 50, paddingLeft: 50 }}>
@@ -44,6 +62,13 @@ export default class HomeScreen extends Component {
           <Text style={{ paddingTop: 50, paddingLeft: 50, color: '#FF0000' }}>
             {' '}
             Click me to turn the device off{' '}
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.handleGetRequest.bind(this)}>
+          <Text style={{ paddingTop: 50, paddingLeft: 50, color: '#FF0000' }}>
+            {' '}
+            Click me to make GET request{' '}
           </Text>
         </TouchableOpacity>
       </View>
